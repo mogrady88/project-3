d8189fa8dcec6464ab8fe78bae196495ba86cfab
Tyler Maxwell <tylermaxwell1990@gmail.com>
2018-10-13 13:10:18 -0400

    test crud threads

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -2,28 +2,28 @@ const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
-    db.Post.find(req.query)
+    db.Comment.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Post.findById(req.params.id)
+    db.Comment.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Post.create(req.body)
+    db.Comment.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Post.findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.Comment.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Post.findById({ _id: req.params.id })
+    db.Comment.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
