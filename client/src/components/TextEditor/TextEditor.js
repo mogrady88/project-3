f5fc6d6d9ea03a840b59a3894475241e9c3a778c
Brian Burch <Bburch05@gmail.com>
2018-10-18 20:54:50 -0400

    asdfasdfasdf

--- client/src/components/TextEditor/TextEditor.js
+++ client/src/components/TextEditor/TextEditor.js
@@ -1,32 +1,87 @@
-import React from 'react'
-import {EditorState, RichUtils} from 'draft-js';
-import Editor from 'draft-js-plugins-editor';
-import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
+import React from "react";
+import { Editor, EditorState, RichUtils } from "draft-js";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
+class PageContainer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			editorState: EditorState.createEmpty()
+		};
+	}
+
+	onChange = editorState => {
+		this.setState({
+			editorState
+		});
+	};
+
+	handleKeyCommand = command => {
+		const newState = RichUtils.handleKeyCommand(
+			this.state.editorState,
+			command
+		);
+		if (newState) {
+			this.onChange(newState);
+			return "handled";
+		}
+		return "not-handled";
+	};
+
+	onUnderlineClick = () => {
+		this.onChange(
+			RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+		);
+	};
+
+	onBoldClick = () => {
+		this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
+	};
+
+	onItalicClick = () => {
+		this.onChange(
+			RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
+		);
+  };
+  
+  onH1Click = () => {
+    this.onChange(
+      RichUtils.toggleBlockType(this.state.editorState, "header-one")
+    )
   }
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(
-      this.state.editorState,
-      'BOLD'
-    ));
+  onH2Click = () => {
+    this.onChange(
+      RichUtils.toggleBlockType(this.state.editorState, "header-two")
+    )
   }
-  render() {
-    return (
-      <div id='content'>
-        <h1>Draft.js Editor</h1>
-        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-        <div className='editor'>
-          <Editor
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-          />
-        </div>
-      </div>
-    );
+  onH3Click = () => {
+    this.onChange(
+      RichUtils.toggleBlockType(this.state.editorState, "header-three")
+    )
   }
+
+	render() {
+		return (
+			<div className="editorContainer">
+				<button onClick={this.onUnderlineClick}>U</button>
+				<button onClick={this.onBoldClick}>
+					<b>B</b>
+				</button>
+				<button onClick={this.onItalicClick}>
+					<em>I</em>
+				</button>
+        <button onClick={this.onH1Click}>H1</button>
+        <button onClick={this.onH2Click}>H2</button>
+        <button onClick={this.onH3Click}>H3</button>
+				<div className="editors">
+					<Editor
+						editorState={this.state.editorState}
+						handleKeyCommand={this.handleKeyCommand}
+						onChange={this.onChange}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
+
+export default PageContainer;
