a34d0732ed7ac4515bea417cac35a70a2fe020eb
disconaked <nolan.klinke@gmail.com>
2018-10-22 18:24:53 -0400

    styled log in and task on private page

--- client/src/components/privateComponents/Task/Task.js
+++ client/src/components/privateComponents/Task/Task.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { Row, Col, Card, Input, Button } from "react-materialize";
+import { Button } from "react-materialize";
 import "./Task.css";
+import Row from "../../grid/Row";
+import Col from "../../grid/Col";
 
 class Task extends React.Component {
   render() {
     return (
-      <Card
-        className="blue-grey lighten-1"
-        textClassName="white-text"
-        title={this.props.title}
-      >
-        <Row>
-          <Col s={8}>{this.props.summary}</Col>
-          <Col s={4}>
-            <p>Status: {this.props.status}</p>
-            <p>Funds: ${this.props.funds}</p>
-            <Button>Edit task</Button>
-          </Col>
-        </Row>
-      </Card>
+      <div className="card">
+        <div className="card-content taskCard">
+          <span className="card-title">{this.props.title}</span>
+          <Row>
+            <Col size="8">{this.props.summary}</Col>
+            <Col otherClasses="tastCardCol" size="4">
+              <p>Status: {this.props.status}</p>
+              <p>Funds: ${this.props.funds}</p>
+              <Button>Edit task</Button>
+            </Col>
+          </Row>
+        </div>
+      </div>
     );
   }
 }
