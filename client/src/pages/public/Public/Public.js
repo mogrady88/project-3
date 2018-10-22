9fca2476451bffc07071ad9243269b20e2438a88
Tyler Maxwell <tylermaxwell1990@gmail.com>
2018-10-22 13:42:47 -0400

    cleanup complete

--- client/src/pages/public/Public/Public.js
+++ client/src/pages/public/Public/Public.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
-<<<<<<< HEAD:client/src/pages/public/Public/Public.js
 import API from "../../../utils/postsAPI";
 import Nav from "../../../components/shared/Nav";
 import PostCard from "../../../components/public/PostCard";
 import Row from "../../../components/shared/grid/Row";
 import Col from "../../../components/shared/grid/Col";
-=======
-import API from "../../utils/postsAPI";
-import { Link } from "react-router-dom";
-import Nav from "../../components/Nav";
-import Row from "react-materialize/lib/Row";
-import Col from "react-materialize/lib/Col";
-import PostCard from "../../components/PostCard";
->>>>>>> master:client/src/pages/Public/Public.js
 
 class Public extends Component {
   state = {
@@ -42,11 +33,7 @@ class Public extends Component {
         <Nav isPublic={true} />
         <div className="container">
           <Row>
-<<<<<<< HEAD:client/src/pages/public/Public/Public.js
             <Col size="12" className="grid1">
-=======
-            <Col s={12} className="grid1">
->>>>>>> master:client/src/pages/Public/Public.js
               {this.state.posts.map(post => (
                 <PostCard {...post} isPublic={true} />
               ))}
