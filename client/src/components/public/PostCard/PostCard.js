21c4a25d59d61d8a6b9e1d1c67f0c32ca50f8bd4
Tyler Maxwell <tylermaxwell1990@gmail.com>
2018-10-25 19:37:04 -0400

    time formatting added

--- client/src/components/public/PostCard/PostCard.js
+++ client/src/components/public/PostCard/PostCard.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Row from "../../shared/grid/Row";
 import Col from "../../shared/grid/Col";
+import Moment from "../../shared/Moment";
 import "./PostCard.css";
 
 const PostCard = props => (
   <div className="postcard">
     <h4>{props.title}</h4>
+    <p>
+      <Moment time={props.updatedAt} />
+    </p>
     <Row>
       <Col size="12">Tags: {props.tags.join(", ")}</Col>
     </Row>
