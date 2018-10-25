f965b296074c96c041d4dcfeac0b483f217c2e50
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-10-24 20:41:58 -0400

    refresh works

--- client/src/components/private/PrivateRoute/PrivateRoute.js
+++ client/src/components/private/PrivateRoute/PrivateRoute.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import UsersAPI from "../../../utils/usersAPI";
 
 const PrivateRoute = ({
   component: Component,
@@ -10,15 +9,6 @@ const PrivateRoute = ({
   user,
   getUser
 }) => {
-  // const checkAuth = () => {
-  //   UsersAPI.getCurrentUser().then(response => {
-  //     console.log("Get user response: ");
-  //     console.log(response.data);
-  //     console.log("Is user authenticated?", response.data.authenticated);
-  //     return response.data.authenticated;
-  //   });
-  // };
-  // const loggedIn = this.props.loggedIn; // user is/is not authenticated
   let session = sessionStorage.getItem("disco-panda");
   console.log("PrivateRoute.js session: " + session);
   console.log("PrivateRoute.js says loggedIn is " + loggedIn);
