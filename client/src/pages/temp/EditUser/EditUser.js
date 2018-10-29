c10db588af0abc7f05c5f1fc8e5ca11db681eb34
Tyler Maxwell <tylermaxwell1990@gmail.com>
2018-10-29 13:16:15 -0400

    merge

--- client/src/pages/temp/EditUser/EditUser.js
+++ client/src/pages/temp/EditUser/EditUser.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Card, Tabs, Tab, Input, Button } from "react-materialize";
+import Col from "../../../components/shared/grid/Col";
 import { Redirect } from "react-router-dom";
 import Nav from "../../../components/shared/Nav";
 import axios from "axios";
@@ -8,82 +8,74 @@ import UsersAPI from "../../../utils/usersAPI";
 import UserList from "../../../components/EditUserList";
 
 class ViewUsers extends Component {
-
-state = {
-        users: [],
-        user: {
-            username: "",
-            password: "",
-            firstName: "",
-            lastName: "",
-            email: ""
-        }
-}
-
-componentDidMount(){
-    UsersAPI.getAllUsers()
-    .then(res => {
-        this.setState({ users: res.data })
-    })
-    .catch(err => console.log(err));
+  state = {
+    users: [],
+    user: {
+      username: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      email: ""
     }
+  };
 
-loadUsers(){
+  componentDidMount() {
     UsersAPI.getAllUsers()
-    .then(res => {
-        this.setState({ users: res.data })
-    })
-    .catch(err => console.log(err));
-    }
+      .then(res => {
+        this.setState({ users: res.data });
+      })
+      .catch(err => console.log(err));
+  }
 
+  loadUsers() {
+    UsersAPI.getAllUsers()
+      .then(res => {
+        this.setState({ users: res.data });
+      })
+      .catch(err => console.log(err));
+  }
 
-updateUserInfo(id, data){
-    UsersAPI.updateUser(id, data)
-    .then(res => 
-        console.log(res.data)
-    )
-}
+  updateUserInfo(id, data) {
+    UsersAPI.updateUser(id, data).then(res => console.log(res.data));
+  }
 
-handleOnDisable = (event) => {
+  handleOnDisable = event => {
     event.preventDefault();
+  };
 
-    }
+  handleOnChange = event => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    this.setState({
+      user: {
+        ...this.state.user,
+        [name]: value
+      }
+    });
+  };
 
-handleOnChange = (event) => {
-  
-        event.preventDefault();
-        const { name, value } = event.target;
-        this.setState({
-            user: {
-                ...this.state.user,
-                [name]: value
-            }
-    })
-}
+  handleOnClick = event => {
+    console.log(event.target);
+    event.preventDefault();
+    const userID = event.target.value;
+    console.log(userID);
+    this.updateUserInfo(userID, this.state.user);
+  };
 
-    handleOnClick = (event) => {
-        console.log(event.target);
-        event.preventDefault();
-        const userID = event.target.value;
-        console.log(userID);
-        this.updateUserInfo(userID, this.state.user);
-  
-    };
-
-
-    render(){
-        return(
-            <div className="container">
-            <Row>
-                <div>
-                {this.state.users.map(user => (
-                <UserList user={user} onDisable={this.handleOnDisable} onChange={this.handleOnChange} onClick={this.handleOnClick}/>
-                ))}
-                </div>
-            </Row>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Col size="9">
+        {this.state.users.map(user => (
+          <UserList
+            user={user}
+            onDisable={this.handleOnDisable}
+            onChange={this.handleOnChange}
+            onClick={this.handleOnClick}
+          />
+        ))}
+      </Col>
+    );
+  }
 }
 
 export default ViewUsers;
